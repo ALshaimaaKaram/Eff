67258a861808e648813874d9d76879209fe8902c
Alshaimaa Karam <alshaimaak.cs@gmail.com>
2021-09-28 15:07:42 +0200

    Add project files.

---
+++ Eff/Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿
+namespace Eff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbDept = new System.Windows.Forms.ComboBox();
+            this.lbxData = new System.Windows.Forms.ListBox();
+            this.txtSSN = new System.Windows.Forms.TextBox();
+            this.lblSSN = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtSalary = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.grdData = new System.Windows.Forms.DataGridView();
+            this.fnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sSNDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bdateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.addressDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salaryDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dnoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmbDept
+            // 
+            this.cmbDept.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.cmbDept.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDept.FormattingEnabled = true;
+            this.cmbDept.Location = new System.Drawing.Point(90, 36);
+            this.cmbDept.Name = "cmbDept";
+            this.cmbDept.Size = new System.Drawing.Size(205, 28);
+            this.cmbDept.TabIndex = 0;
+            this.cmbDept.SelectedIndexChanged += new System.EventHandler(this.cmbDept_SelectedIndexChanged);
+            // 
+            // lbxData
+            // 
+            this.lbxData.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbxData.FormattingEnabled = true;
+            this.lbxData.ItemHeight = 20;
+            this.lbxData.Location = new System.Drawing.Point(90, 101);
+            this.lbxData.Name = "lbxData";
+            this.lbxData.Size = new System.Drawing.Size(205, 224);
+            this.lbxData.TabIndex = 1;
+            this.lbxData.SelectedIndexChanged += new System.EventHandler(this.lbxData_SelectedIndexChanged);
+            // 
+            // txtSSN
+            // 
+            this.txtSSN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSSN.Location = new System.Drawing.Point(469, 48);
+            this.txtSSN.Name = "txtSSN";
+            this.txtSSN.Size = new System.Drawing.Size(170, 26);
+            this.txtSSN.TabIndex = 2;
+            // 
+            // lblSSN
+            // 
+            this.lblSSN.AutoSize = true;
+            this.lblSSN.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSSN.Location = new System.Drawing.Point(670, 48);
+            this.lblSSN.Name = "lblSSN";
+            this.lblSSN.Size = new System.Drawing.Size(45, 20);
+            this.lblSSN.TabIndex = 3;
+            this.lblSSN.Text = "SSN";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(368, 299);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(100, 40);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtFname
+            // 
+            this.txtFname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFname.Location = new System.Drawing.Point(469, 101);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(170, 26);
+            this.txtFname.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(670, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fname";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLname.Location = new System.Drawing.Point(469, 163);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(170, 26);
+            this.txtLname.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(670, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Lname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(670, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 20);
+            this.label4.TabIndex = 3;
+            // 
+            // txtSalary
+            // 
+            this.txtSalary.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSalary.Location = new System.Drawing.Point(469, 223);
+            this.txtSalary.Name = "txtSalary";
+            this.txtSalary.Size = new System.Drawing.Size(170, 26);
+            this.txtSalary.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(670, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 20);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Salary";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(494, 299);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(100, 40);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(615, 299);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(100, 40);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Dept";
+            // 
+            // grdData
+            // 
+            this.grdData.AutoGenerateColumns = false;
+            this.grdData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.fnameDataGridViewTextBoxColumn,
+            this.lnameDataGridViewTextBoxColumn,
+            this.sSNDataGridViewTextBoxColumn,
+            this.bdateDataGridViewTextBoxColumn,
+            this.addressDataGridViewTextBoxColumn,
+            this.salaryDataGridViewTextBoxColumn,
+            this.dnoDataGridViewTextBoxColumn});
+            this.grdData.DataSource = this.employeeBindingSource;
+            this.grdData.Location = new System.Drawing.Point(12, 359);
+            this.grdData.Name = "grdData";
+            this.grdData.Size = new System.Drawing.Size(742, 191);
+            this.grdData.TabIndex = 5;
+            this.grdData.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdData_CellContentClick);
+            this.grdData.SelectionChanged += new System.EventHandler(this.grdData_SelectionChanged);
+            // 
+            // fnameDataGridViewTextBoxColumn
+            // 
+            this.fnameDataGridViewTextBoxColumn.DataPropertyName = "Fname";
+            this.fnameDataGridViewTextBoxColumn.HeaderText = "Fname";
+            this.fnameDataGridViewTextBoxColumn.Name = "fnameDataGridViewTextBoxColumn";
+            // 
+            // lnameDataGridViewTextBoxColumn
+            // 
+            this.lnameDataGridViewTextBoxColumn.DataPropertyName = "Lname";
+            this.lnameDataGridViewTextBoxColumn.HeaderText = "Lname";
+            this.lnameDataGridViewTextBoxColumn.Name = "lnameDataGridViewTextBoxColumn";
+            // 
+            // sSNDataGridViewTextBoxColumn
+            // 
+            this.sSNDataGridViewTextBoxColumn.DataPropertyName = "SSN";
+            this.sSNDataGridViewTextBoxColumn.HeaderText = "SSN";
+            this.sSNDataGridViewTextBoxColumn.Name = "sSNDataGridViewTextBoxColumn";
+            // 
+            // bdateDataGridViewTextBoxColumn
+            // 
+            this.bdateDataGridViewTextBoxColumn.DataPropertyName = "Bdate";
+            this.bdateDataGridViewTextBoxColumn.HeaderText = "Bdate";
+            this.bdateDataGridViewTextBoxColumn.Name = "bdateDataGridViewTextBoxColumn";
+            // 
+            // addressDataGridViewTextBoxColumn
+            // 
+            this.addressDataGridViewTextBoxColumn.DataPropertyName = "Address";
+            this.addressDataGridViewTextBoxColumn.HeaderText = "Address";
+            this.addressDataGridViewTextBoxColumn.Name = "addressDataGridViewTextBoxColumn";
+            // 
+            // salaryDataGridViewTextBoxColumn
+            // 
+            this.salaryDataGridViewTextBoxColumn.DataPropertyName = "Salary";
+            this.salaryDataGridViewTextBoxColumn.HeaderText = "Salary";
+            this.salaryDataGridViewTextBoxColumn.Name = "salaryDataGridViewTextBoxColumn";
+            // 
+            // dnoDataGridViewTextBoxColumn
+            // 
+            this.dnoDataGridViewTextBoxColumn.DataPropertyName = "Dno";
+            this.dnoDataGridViewTextBoxColumn.HeaderText = "Dno";
+            this.dnoDataGridViewTextBoxColumn.Name = "dnoDataGridViewTextBoxColumn";
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataSource = typeof(Eff.Employee);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(889, 547);
+            this.Controls.Add(this.grdData);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSSN);
+            this.Controls.Add(this.txtSalary);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.txtSSN);
+            this.Controls.Add(this.lbxData);
+            this.Controls.Add(this.cmbDept);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grdData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDept;
+        private System.Windows.Forms.ListBox lbxData;
+        private System.Windows.Forms.TextBox txtSSN;
+        private System.Windows.Forms.Label lblSSN;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSalary;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView grdData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sSNDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bdateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn addressDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn salaryDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dnoDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+    }
+}
+
---
+++ Eff/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Eff
+{
+    public partial class Form1 : Form
+    {
+        CompanyEntities context = new CompanyEntities();
+        int Dno;
+        public Form1()
+        {
+            InitializeComponent();
+
+            cmbDept.DisplayMember = "Dname";
+            cmbDept.ValueMember = "Dnum";
+            cmbDept.DataSource = context.Departments.Select(d => d).ToList();
+        }
+
+        private void RefreshData()
+        {
+            lbxData.DisplayMember = "Fname";
+
+            Dno = (int)cmbDept.SelectedValue;
+            lbxData.DataSource = context.Employees.Where(em => em.Dno == Dno).ToList();
+        }
+
+        private void RefreshGrid()
+        {
+            Dno = (int)cmbDept.SelectedValue;
+            grdData.DataSource = context.Employees.Where(em => em.Dno == Dno).ToList();
+        }
+
+        private void cmbDept_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            RefreshData();  
+            RefreshGrid();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+           
+            var employee = new Employee()
+            {
+                SSN = Int32.Parse(txtSSN.Text),
+                Fname = txtFname.Text,
+                Lname = txtLname.Text,
+                Salary = Int32.Parse(txtSalary.Text),
+                Dno = (int)cmbDept.SelectedValue
+            };
+            context.Employees.Add(employee);
+            context.SaveChanges();
+
+            RefreshGrid();
+            RefreshData();
+
+            MessageBox.Show("Added Success");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            
+            Employee empOLd = (Employee)lbxData.SelectedItem;
+            
+            int SSN = empOLd.SSN;
+
+            Employee employee = context.Employees.Where(em => em.SSN == SSN).First();
+
+            employee.SSN = Int32.Parse(txtSSN.Text);
+            employee.Fname = txtFname.Text;
+            employee.Lname = txtLname.Text;
+            employee.Salary = Int32.Parse(txtSalary.Text);
+            employee.Dno = (int)cmbDept.SelectedValue;
+                
+            context.SaveChanges();
+            RefreshData();
+            RefreshGrid();
+            MessageBox.Show("Updated Success");
+
+        }
+
+        private void lbxData_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Employee emp = (Employee)lbxData.SelectedItem;
+
+            txtSSN.Text = emp.SSN.ToString();
+            txtFname.Text = emp.Fname;
+            txtLname.Text = emp.Lname;
+            txtSalary.Text = emp.Salary.ToString();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int SSN = Int32.Parse(txtSSN.Text);
+            var emp = context.Employees.Where(em => em.SSN == SSN).FirstOrDefault();
+            context.Employees.Remove(emp);
+            context.SaveChanges();
+
+            RefreshData();
+            RefreshGrid();
+            MessageBox.Show("deleted Success");
+        }
+
+        private void grdData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void grdData_SelectionChanged(object sender, EventArgs e)
+        {
+            Employee emp = (Employee)grdData.CurrentRow.DataBoundItem;
+
+            txtSSN.Text = emp.SSN.ToString();
+            txtFname.Text = emp.Fname;
+            txtLname.Text = emp.Lname;
+            txtSalary.Text = emp.Salary.ToString();
+        }
+    }
+}
